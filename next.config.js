df24a0ca8fde7b136e53aff2dad4fd1650c6416a
Dennis Acosta <email@dennisacosta.me>
2022-06-29 14:21:25 -0400

    Clone lazy lambs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = {
+    env: {
+        NEXT_PUBLIC_FORM: 'myyojjde',
+    },
+
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            issuer: {
+                test: /\.(js|ts)x?$/,
+            },
+
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../assets/styles/reset.css';
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <style global jsx>{`
+                :root {
+                    background-color: #000;
+                    margin: 0;
+                    overflow-x: hidden;
+                    padding: 0;
+                    scroll-behavior: smooth;
+                }
+
+                a {
+                    text-decoration: none;
+                }
+            `}</style>
+            <Head>
+                <title>Lazy Lamb</title>
+            </Head>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
