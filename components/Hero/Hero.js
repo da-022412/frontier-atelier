9643f50c7a855e6c0f6220b24524cb82b35c91de
Dennis Acosta <email@dennisacosta.me>
2022-07-06 19:05:35 -0400

    Add timed state change

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,26 +1,53 @@
+import { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
-
-import Heading from '../Heading';
 
 import heroStyles from './Hero.module.scss';
 
 import Container from '../Container';
+import Heading from '../Heading';
 
 import arrowDown from '../../assets/images/arrow-down.webp';
 
 const Hero = () => {
+    const [time, setTime] = useState(false);
+
+    useEffect(() => {
+        let timer = setTimeout(() => setTime(true), 5000);
+        return () => {
+            clearTimeout(timer);
+        };
+    }, []);
+
     return (
         <div className={heroStyles.hero}>
-            <div className={`${heroStyles['heading-container']}`}>
-                <Container>
-                    <Heading style='heading-1' level='1'>
-                        Welcome to the{' '}
-                        <span style={{ color: 'var(--color-accent)' }}>
-                            curiosity sandbox
-                        </span>
-                    </Heading>
-                </Container>
-            </div>
+            {!time ? (
+                <div className={`${heroStyles['heading-container']}`}>
+                    <Container>
+                        <Heading style='heading-2' level='2'>
+                            Welcome to the{' '}
+                            <span style={{ color: 'var(--color-accent)' }}>
+                                curiosity sandbox
+                            </span>
+                        </Heading>
+                    </Container>
+                </div>
+            ) : (
+                <div className={`${heroStyles['heading-container']}`}>
+                    <Container>
+                        <Heading style='heading-1' level='1'>
+                            <span style={{ color: 'var(--color-accent)' }}>
+                                Frontier Atelier
+                            </span>{' '}
+                            is a bespoke, private online luxury shopping
+                            platform enabling patrons to purchase one-of-one
+                            items and experiences curated by creators and
+                            artisans from the far reaches of this this beautiful
+                            world.
+                        </Heading>
+                    </Container>
+                </div>
+            )}
             <video autoPlay loop muted className={heroStyles.video}>
                 <source src='/assets/videos/FA_hero-web.mp4' type='video/mp4' />
             </video>
