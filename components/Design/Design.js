9e4015667236a3d064b1dd76ef8b7b47d3719f4a
Dennis Acosta <email@dennisacosta.me>
2022-07-11 19:35:51 -0400

    Update team component

--- components/Design/Design.js
+++ components/Design/Design.js
@@ -1,3 +1,5 @@
+import { useState, useCallback, useEffect } from 'react';
+
 import Image from 'next/image';
 
 import designStyles from './Design.module.scss';
@@ -6,8 +8,37 @@ import Container from '../Container';
 import Heading from '../Heading';
 
 import Infographic from '../../assets/images/Infographic_Design101.svg';
+import InfographicMobile from '../../assets/images/ig_design101_mobile 1.webp';
+
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback((e) => {
+        if (e.matches) {
+            setTargetReached(true);
+        } else {
+            setTargetReached(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`);
+        media.addEventListener('change', updateTarget);
+
+        // Check on mount (callback is not called until a change occurs)
+        if (media.matches) {
+            setTargetReached(true);
+        }
+
+        return () => media.removeEventListener('change', updateTarget);
+    }, []);
+
+    return targetReached;
+};
 
 const Design = () => {
+    const isBreakpoint = useMediaQuery(767);
+
     return (
         <Container>
             <div className={`${designStyles['design-container']}`}>
@@ -17,9 +48,15 @@ const Design = () => {
                         Design School 101
                     </span>
                 </Heading>
-                <figure className={`${designStyles['design-img']}`}>
-                    <Image src={Infographic} alt='Infographic' />
-                </figure>
+                {isBreakpoint ? (
+                    <figure className={`${designStyles['design-img']}`}>
+                        <Image src={InfographicMobile} alt='Infographic' />
+                    </figure>
+                ) : (
+                    <figure className={`${designStyles['design-img']}`}>
+                        <Image src={Infographic} alt='Infographic' />
+                    </figure>
+                )}
             </div>
         </Container>
     );
