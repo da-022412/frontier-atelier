3b5d17927015848a687e99263a9ef097cf6a459a
Dennis Acosta <email@dennisacosta.me>
2022-07-12 13:15:46 -0400

    Adjust blurbs

--- components/Blurb/Blurb.js
+++ components/Blurb/Blurb.js
@@ -19,8 +19,8 @@ const Blurb = ({ img, title, copy, mobile }) => {
                     <figure className={`${blurbStyles['blurb-img']}`}>
                         <Image
                             src={img}
-                            width={200}
-                            height={200}
+                            width={400}
+                            height={400}
                             alt='Profile Pic'
                         />
                     </figure>
@@ -36,8 +36,8 @@ const Blurb = ({ img, title, copy, mobile }) => {
                     <figure className={`${blurbStyles['blurb-img']}`}>
                         <Image
                             src={img}
-                            width={200}
-                            height={200}
+                            width={400}
+                            height={400}
                             alt='Profile Pic'
                         />
                     </figure>
